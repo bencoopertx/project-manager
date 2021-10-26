c265d830d8cc779da40a216758afe00eb1d70bbb
Ben Cooper <78227837+bencooper01@users.noreply.github.com>
2021-10-25 21:32:10 -0700

    feat: cards now draggable

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,16 +2,38 @@ import { IconButton, Text } from "@chakra-ui/react";
 import React from "react";
 import { chakra, Box, Flex, useColorModeValue, Link } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
+import { useDrag } from "react-dnd";
 
 interface Props {
 	text: string;
 }
+const knightStyle: CSSProperties = {};
 
 export const Card: React.FC<Props> = (props) => {
 	const { text } = props;
+	const [{ isDragging }, drag, preview] = useDrag(
+		() => ({
+			type: "card",
+			collect: (monitor) => ({
+				isDragging: !!monitor.isDragging(),
+			}),
+		}),
+		[]
+	);
+
 	return (
-		<Flex p={0} w="full" alignItems="center" justifyContent="center">
-			<Box w="full" maxW="sm" mx="auto" px={1} py={1} bg={useColorModeValue("white", "gray.800")} borderWidth="1px" rounded="md" _hover={{ backgroundColor: "gray.100" }}>
+		<Flex
+			p={0}
+			w="full"
+			alignItems="center"
+			justifyContent="center"
+			ref={drag}
+			style={{
+				...knightStyle,
+				opacity: isDragging ? 0 : 1,
+			}}
+		>
+			<Box w="full" maxW="sm" mx="auto" px={1} py={1} bg={useColorModeValue("white", "gray.800")} borderWidth="1px" rounded="md" _hover={{ backgroundColor: "gray.50" }}>
 				<Box style={{ float: "right" }}>
 					<IconButton colorScheme="gray" aria-label="Search database" icon={<EditIcon />} size="sm" variant="ghost" />
 				</Box>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider>
-			<Component {...pageProps} />
+			<DndProvider backend={HTML5Backend}>
+				<Component {...pageProps} />
+			</DndProvider>
 		</ChakraProvider>
 	);
 }
