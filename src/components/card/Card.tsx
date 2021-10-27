903867516d7da88a078ea5ae20826d8af80c190c
Ben Cooper <78227837+bencooper01@users.noreply.github.com>
2021-10-26 22:11:06 -0700

    feat: is hovering working

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,15 +2,17 @@ import { IconButton, Text } from "@chakra-ui/react";
 import React from "react";
 import { chakra, Box, Flex, useColorModeValue, Link } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
-import { DropTarget, useDrag } from "react-dnd";
+import { DropTarget, useDrag, ConnectDropTarget, useDrop } from "react-dnd";
+
+const knightStyle: CSSProperties = {};
 
 interface Props {
 	text: string;
 }
-const knightStyle: CSSProperties = {};
 
 export const Card: React.FC<Props> = (props) => {
-	const { text } = props;
+	const { text, connectDropTarget, isShiftedDown } = props;
+	const [bottomPadding, setBottomPadding] = React.useState(0);
 	const [{ isDragging }, drag, preview] = useDrag(
 		() => ({
 			type: "card",
@@ -20,30 +22,47 @@ export const Card: React.FC<Props> = (props) => {
 		}),
 		[]
 	);
+	const ref = React.useRef(null);
+	const [{ canDrop, isOverCurrent }, drop] = useDrop({
+		accept: "card",
+		canDrop: () => true,
+		hover: () => {
+			console.log("is hovering");
+			setBottomPadding(10);
+		},
+		collect: (monitor) => ({
+			hovered: monitor.isOver(),
+			canDrop: monitor.canDrop(),
+			isOverCurrent: monitor.isOver({ shallow: true }),
+		}),
+	});
+	drop(ref);
 
 	return (
-		<Flex
-			p={0}
-			w="full"
-			alignItems="center"
-			justifyContent="center"
-			ref={drag}
-			style={{
-				...knightStyle,
-				opacity: isDragging ? 0 : 1,
-			}}
-		>
-			<Box w="full" maxW="sm" mx="auto" px={1} py={1} bg={useColorModeValue("white", "gray.800")} borderWidth="1px" rounded="md" _hover={{ backgroundColor: "gray.50" }}>
-				<Box style={{ float: "right" }}>
-					<IconButton colorScheme="gray" aria-label="Search database" icon={<EditIcon />} size="sm" variant="ghost" />
+		<div ref={ref}>
+			<Flex
+				p={0}
+				w="full"
+				alignItems="center"
+				justifyContent="center"
+				ref={drag}
+				style={{
+					...knightStyle,
+					opacity: isDragging ? 0 : 1,
+				}}
+				mb={bottomPadding}
+			>
+				<Box w="full" maxW="sm" mx="auto" px={1} py={1} bg={useColorModeValue("white", "gray.800")} borderWidth="1px" rounded="md" _hover={{ backgroundColor: "gray.50" }}>
+					<Box style={{ float: "right" }}>
+						<IconButton colorScheme="gray" aria-label="Search database" icon={<EditIcon />} size="sm" variant="ghost" />
+					</Box>
+					<Box px={1}>
+						<Text>{text}</Text>
+					</Box>
 				</Box>
-				<Box px={1}>
-					<Text>{text}</Text>
-				</Box>
-			</Box>
-		</Flex>
+			</Flex>
+		</div>
 	);
 };
-export default DropTarget("card", { canDrop: () => {}, drop: () => {}, hover: () => {} }, (connect, monitor) => {
-	return {};
-})(Card);
+
+export default Card;
