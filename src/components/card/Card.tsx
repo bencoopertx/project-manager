6e951de3ec328c4a6dfc850a2a9cbc5beffa2b2a
Ben Cooper <78227837+bencooper01@users.noreply.github.com>
2021-10-27 21:42:03 -0700

    feat: passing throught item height on hover

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,25 +10,47 @@ interface Props {
 	text: string;
 }
 
+const handleHover = () => {};
+
 export const Card: React.FC<Props> = (props) => {
 	const { text, connectDropTarget, isShiftedDown } = props;
 	const [bottomPadding, setBottomPadding] = React.useState(0);
+	const [currentHeight, setCurrentHeight] = React.useState(0);
+	const [testRefState, setTestRefState] = React.useState(undefined);
+
+	const ref = React.useRef(null);
+	const ref2 = React.useRef(null);
+
+	React.useEffect(() => {
+		// setting the client height that is passed to useDrop
+		setCurrentHeight(ref.current.clientHeight);
+		setTestRefState(ref);
+	});
+
+	// this is the stuff we send over on drag
 	const [{ isDragging }, drag, preview] = useDrag(
 		() => ({
 			type: "card",
+			item: { id: "hey", item: currentHeight, ref: ref },
 			collect: (monitor) => ({
 				isDragging: !!monitor.isDragging(),
 			}),
 		}),
 		[]
 	);
-	const ref = React.useRef(null);
+
+	// this how we interact w/ stuff sent on drag
 	const [{ canDrop, isOverCurrent }, drop] = useDrop({
 		accept: "card",
 		canDrop: () => true,
-		hover: () => {
-			console.log("is hovering");
-			setBottomPadding(10);
+		hover: (item, monitor) => {
+			const item2 = monitor.getItem();
+			console.log("item", item.ref.current.clientHeight);
+			// get direction of hover.
+
+			// move
+
+			// somehow get rid of adjustments on not hovering
 		},
 		collect: (monitor) => ({
 			hovered: monitor.isOver(),
@@ -51,6 +73,7 @@ export const Card: React.FC<Props> = (props) => {
 					opacity: isDragging ? 0 : 1,
 				}}
 				mb={bottomPadding}
+				itemRef={ref2}
 			>
 				<Box w="full" maxW="sm" mx="auto" px={1} py={1} bg={useColorModeValue("white", "gray.800")} borderWidth="1px" rounded="md" _hover={{ backgroundColor: "gray.50" }}>
 					<Box style={{ float: "right" }}>
